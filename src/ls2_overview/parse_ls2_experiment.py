8ac0b12492412709329f9c2473b525a968b24836
Max Hess <max.hess@fmi.ch>
2025-12-19 19:18:18 +0100

    minor stuff

--- src/ls2_overview/parse_ls2_experiment.py
+++ src/ls2_overview/parse_ls2_experiment.py
@@ -4,17 +4,16 @@ import re
 import json
 import polars as pl
 
+
 experiment_root = Path(
-    r"C:\Users\hessmax\Data\ls2_synthetic\experiment"
+    r"Z:\rawlanding\znascakova\20251209_105714_W1--singlecell--W2-W6--Day3--H2B-mg"
 )
 stack_settings_file = "Settings/STAGE_ZStackSettings.json"
-stack_pattern = re.compile(r"(?P<channel>\S+)_View(?P<view>\d)-T(?P<timepoint>\d+)")
-position_pattern = re.compile(r"(?P<position>[^_]+)_(?P<acquisition>[^_]+)(_(?P<projection>[^_]+))?")
-
-
-# %%
-path = Path(
-    "N:/liberali/rawlanding/znascakova/20251209_105714_W1--singlecell--W2-W6--Day3--H2B-mg/w1-poisition12_1um-150pls/mG_View1-T0072.tif"
+stack_pattern = re.compile(
+    r"(?P<channel>\S+)_(?P<view>View(?P<view_id>\d))-T(?P<t_id>\d+)"
+)
+position_pattern = re.compile(
+    r"(?P<position>[^_]+)_(?P<stack>[^_]+)(_(?P<projection>[^_]+))?"
 )
 
 
@@ -27,19 +26,22 @@ def parse_ls2_experiment(experiment_root: Path):
             **position_pattern.match(rel_path.parent.name).groupdict(),
             **stack_pattern.match(rel_path.stem).groupdict(),
             "path": path.as_posix(),
+            "tif_file_size": path.stat().st_size,
         }
         acc.append(components)
 
-    return pl.DataFrame(
+    df = pl.DataFrame(
         acc,
         schema={
-            "position": pl.Utf8,
-            "acquisition": pl.Utf8,
-            "projection": pl.Utf8,
-            "channel": pl.Utf8,
-            "view": pl.UInt8,
-            "timepoint": pl.UInt16,
-            "path": pl.Utf8,
+            "position": pl.String,
+            "stack": pl.String,
+            "projection": pl.String,
+            "channel": pl.String,
+            "view": pl.String,
+            "view_id": pl.UInt8,
+            "t_id": pl.UInt16,
+            "path": pl.String,
+            "tif_file_size": pl.UInt64,
         },
         strict=False,
     ).with_columns(
@@ -57,37 +59,17 @@ def parse_ls2_experiment(experiment_root: Path):
         .alias("well_position_id"),
     )
 
+    df_vols = df.filter(pl.col("projection").is_null())
+    df_projs = df.filter(pl.col("projection").is_not_null())
+    return df_vols, df_projs
+
 
 # %%
-parse_ls2_experiment(experiment_root)   
+df = parse_ls2_experiment(experiment_root)
 
 # %%
-pl.DataFrame(
-        acc,
-        schema={
-            "position": pl.Utf8,
-            "acquisition": pl.Utf8,
-            "channel": pl.Utf8,
-            "view": pl.UInt8,
-            "timepoint": pl.UInt16,
-            "path": pl.Utf8,
-        },
-        strict=False,
-    ).with_columns(
-        pl.col("position")
-        .str.split("-")
-        .list.get(0)
-        .str.replace_all("\D", "")
-        .cast(pl.UInt8)
-        .alias("well_id"),
-        pl.col("position")
-        .str.split("-")
-        .list.get(1)
-        .str.replace_all("\D", "")
-        .cast(pl.UInt8)
-        .alias("well_position_id"),
-    ).rename({"timepoint": "t_id", "view": "view_id"})
-
-# %%
-with open(experiment_root / stack_settings_file) as f:
-    stack_settings = json.load(f)
+print(
+    df.filter(pl.col("projection").is_null())
+    .group_by("position", "stack", "view", "channel", maintain_order=True)
+    .agg(pl.len())
+)
