f998b46aa224800121db4c285b7f273fe5442f6b
Max Hess <max.hess@fmi.ch>
2025-12-22 10:13:50 +0100

    clean up

--- src/ls2_overview/parse_ls2_experiment.py
+++ src/ls2_overview/parse_ls2_experiment.py
@@ -1,30 +1,34 @@
-# %%
 from pathlib import Path
 import re
-import json
 import polars as pl
+import sys
 
-
-experiment_root = Path(
-    r"Z:\rawlanding\znascakova\20251209_105714_W1--singlecell--W2-W6--Day3--H2B-mg"
-)
-stack_settings_file = "Settings/STAGE_ZStackSettings.json"
-stack_pattern = re.compile(
+STACK_PATTERN = re.compile(
     r"(?P<channel>\S+)_(?P<view>View(?P<view_id>\d))-T(?P<t_id>\d+)"
 )
-position_pattern = re.compile(
+POSITION_PATTERN = re.compile(
     r"(?P<position>[^_]+)_(?P<stack>[^_]+)(_(?P<projection>[^_]+))?"
 )
 
 
-# %%
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python parse_ls2_experiment.py <experiment_root>")
+        sys.exit(1)
+    experiment_root = Path(sys.argv[1])
+    df_vols, df_projs = parse_ls2_experiment(experiment_root)
+    print(df_vols)
+    print(df_projs)
+    sys.exit(0)
+
+
 def parse_ls2_experiment(experiment_root: Path):
     acc = []
     for path in experiment_root.rglob("*.tif"):
         rel_path = path.relative_to(experiment_root)
         components = {
-            **position_pattern.match(rel_path.parent.name).groupdict(),
-            **stack_pattern.match(rel_path.stem).groupdict(),
+            **POSITION_PATTERN.match(rel_path.parent.name).groupdict(),
+            **STACK_PATTERN.match(rel_path.stem).groupdict(),
             "path": path.as_posix(),
             "tif_file_size": path.stat().st_size,
         }
@@ -64,12 +68,5 @@ def parse_ls2_experiment(experiment_root: Path):
     return df_vols, df_projs
 
 
-# %%
-df, df_projs = parse_ls2_experiment(experiment_root)
-
-# %%
-print(
-    df.filter(pl.col("projection").is_null())
-    .group_by("position", "stack", "view", "channel", maintain_order=True)
-    .agg(pl.len())
-)
+if __name__ == "__main__":
+    main()
