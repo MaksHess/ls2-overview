f2fa941b89a9bcfbc5072c53fa9d67125be0bba5
Max Hess <max.hess@fmi.ch>
2025-12-22 19:19:20 +0100

    allow channel selection & ensure order

--- src/ls2_overview/compute_thumbnails.py
+++ src/ls2_overview/compute_thumbnails.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import click
 import dask.array as da
 import ngff_zarr as nz
+import polars as pl
 from dask.array.image import imread
 from zarr.codecs import BloscCodec
 
@@ -17,18 +18,42 @@ COMPRESSION_CODEC = BloscCodec()  # Default: 'zstd', clevel=5
     "-ds", "--down-sample-factor", default=8, help="Down sample factor for thumbnails."
 )
 @click.option("-xy", "--scale-xy", default=1.0, help="Pixel scale x & y.")
-def main(path: str, down_sample_factor: int, scale_xy: float):
+@click.option(
+    "-c",
+    "--channels",
+    default=("*",),
+    multiple=True,
+    help="Channels to use in thumbnails.",
+)
+def main(path: str, down_sample_factor: int, scale_xy: float, channels: str):
     experiment_path = Path(path)
     output_path = experiment_path / "_thumbnails"
     _, df_projections = parse_ls2_experiment(experiment_path)
+    if channels == ("*",):
+        channels = tuple(df_projections["channel"].unique().sort())
+    else:
+        df_projections = df_projections.filter(pl.col("channel").is_in(channels))
+    df_projections = df_projections.with_columns(
+        pl.col("channel")
+        .replace(dict(e[::-1] for e in enumerate(channels)), return_dtype=pl.UInt8)
+        .alias("channel_id")
+    )
     for (acquisition, view), df in df_projections.group_by(
         "acquisition", "view", maintain_order=True
     ):
         out_name = output_path / acquisition / f"{view}.ome.zarr"
         channel_arrs = []
-        for (channel,), df_ch in df.group_by("channel", maintain_order=True):
-            channel_arrs.append(da.stack([imread(e) for e in df_ch["path"]]))
-
+        for (channel,), df_ch in df.sort(by=["channel_id", "t_id"]).group_by(
+            "channel", maintain_order=True
+        ):
+            timepoints = []
+            for i, path in enumerate(df_ch["path"]):
+                t_arr = imread(path)
+                if i > 0:
+                    if t_arr.shape != timepoints[0].shape:
+                        t_arr = da.zeros_like(timepoints[0])
+                timepoints.append(t_arr)
+            channel_arrs.append(da.stack(timepoints))
         arr = da.concatenate(channel_arrs, axis=1)
 
         ngff_image = nz.to_ngff_image(
