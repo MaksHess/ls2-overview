a2e1a51bea6de360c8690b179544e98b22a6921b
Max Hess <max.hess@fmi.ch>
2026-03-10 09:13:11 +0100

    extract parameters to toml flie

--- src/ls2_overview/copy_sample_ls2_dataset.py
+++ src/ls2_overview/copy_sample_ls2_dataset.py
@@ -1,47 +1,63 @@
 import re
 import shutil
+import tomllib
+from argparse import ArgumentParser
 from pathlib import Path
 
 from tqdm import tqdm
 
-TIMEPOINTS = range(108, 116)
-POSITIONS = ["w1-poisition3_1um-150pls", "w3-poisition1_1um-150pls"]
-SOURCE_DIR = Path(
-    r"N:\liberali\rawlanding\znascakova\20251209_105714_W1--singlecell--W2-W6--Day3--H2B-mg"
-)
-TARGET_DIR = Path(r"N:\liberali\rawlanding\hessmax\20251212_1200_SampleExperiment")
-RESET_TIME_INDEX = True
-T_PATTERN = re.compile(r"-T(\d{4})")
-
-
 def main():
-    global_settings_dir = SOURCE_DIR / "Settings"
+    parser = ArgumentParser(description="Copy a sample subset of an LS2 dataset.")
+    parser.add_argument("params", type=Path, help="Path to TOML parameter file.")
+    args = parser.parse_args()
+
+    with open(args.params, "rb") as f:
+        params = tomllib.load(f)
+
+    timepoints = params["timepoints"]
+    positions = params["positions"]
+    stack_settings = params["stack_settings"]
+    source_dir = Path(params["source_dir"])
+    target_dir = Path(params["target_dir"])
+    reset_time_index = params["reset_time_index"]
+    t_pattern = re.compile(params["t_pattern"])
+    t_fstring = params["t_fstring"]
+
+    global_settings_dir = source_dir / "Settings"
 
     ome_tiff_fns = []
     ome_tiff_companions = []
 
-    for pos in POSITIONS:
-        for t in TIMEPOINTS:
-            ome_tiff_fns.extend(SOURCE_DIR.glob(f"{pos}*/*-T{t:04d}.tif"))
-        ome_tiff_companions.extend(SOURCE_DIR.glob(f"{pos}*/*.ome"))
+    for pos in positions:
+        for stack_setting in stack_settings:
+            for t in timepoints:
+                ome_tiff_fns.extend(
+                    source_dir.glob(f"{pos}_{stack_setting}*/t{t:04d}*.tif")
+                )
+            ome_tiff_companions.extend(source_dir.glob(f"{pos}_{stack_setting}*/*.ome"))
 
     shutil.copytree(
         global_settings_dir,
-        TARGET_DIR / global_settings_dir.relative_to(SOURCE_DIR),
+        target_dir / global_settings_dir.relative_to(source_dir),
         dirs_exist_ok=True,
     )
     for fn in ome_tiff_companions:
-        target_path = TARGET_DIR / fn.relative_to(SOURCE_DIR)
+        target_path = target_dir / fn.relative_to(source_dir)
         target_path.parent.mkdir(exist_ok=True)
         shutil.copy2(fn, target_path)
 
     for fn in tqdm(ome_tiff_fns):
-        target_path = TARGET_DIR / fn.relative_to(SOURCE_DIR)
+        target_path = target_dir / fn.relative_to(source_dir)
         target_path.parent.mkdir(exist_ok=True)
-        if RESET_TIME_INDEX:
-            t_index = int(T_PATTERN.search(fn.name).groups()[0])
-            new_t_index = t_index - min(TIMEPOINTS) + 1
-            new_name = re.sub(T_PATTERN, f"-T{new_t_index:04d}", target_path.name)
+        if reset_time_index:
+            mo = t_pattern.search(fn.name)
+            if mo is None:
+                raise ValueError("t_pattern not found in filename.")
+            t_index = int(mo.groups()[0])
+            new_t_index = timepoints.index(t_index) + 1
+            new_name = re.sub(
+                t_pattern, t_fstring.format(new_t_index), target_path.name
+            )
             target_path = target_path.with_name(new_name)
         shutil.copy2(fn, target_path)
 
